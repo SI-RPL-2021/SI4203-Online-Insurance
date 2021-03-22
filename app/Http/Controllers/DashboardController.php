d6ef9dfc9e10266cf58bb34743ff06c019a235e8
Fildzaahns <sitifildzaah@gmail.com>
2021-03-22 13:59:35 +0700

    menambahkan function home ref #3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\view;
 
 class DashboardController extends Controller
 {
-    //
+    public function home (){
+        return view('dashboard/home');
+    }
 }
