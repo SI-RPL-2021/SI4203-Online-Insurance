8c0353737ebdb3fa301e173d421b5e78c2371874
Nurfadilah13 <dillanurfa13@gmail.com>
2021-03-27 00:23:55 +0700

    Menyelesaikan CRUD Subscriptions ref #11

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\view;
 
 class DashboardController extends Controller
 {
-    public function home (){
+    public function home ()
+    {
         return view('dashboard/home');
     }
 }
