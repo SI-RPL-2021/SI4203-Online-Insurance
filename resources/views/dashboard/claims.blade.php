00f991fe25033e3d1b9d9a21178dd69dbaa3d36e
Fildzaahns <sitifildzaah@gmail.com>
2021-03-27 00:22:05 +0700

    menyelesaikan crud dashboard ref #3

--- resources/views/dashboard/claims.blade.php
+++ resources/views/dashboard/claims.blade.php
@@ -4,6 +4,13 @@
 @section('content')
 
 <div class="container pb-5">
+
   {{-- Content --}}
+  @foreach($claims as $claim)
+  <p>{{ $claim->status }}</p>
+  <p>{{ $claim->note }}</p>
+  <p>{{ $claim->amount }}</p>
+
+  @endforeach
 </div>
 @endsection
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,3 +4,8 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'home'])->name("dashboard.home");
+Route::get('/claims', [DashboardController::class, 'claims'])->name("dashboard.claims");
+Route::get('/policies', [DashboardController::class, 'policies'])->name("dashboard.policies");
+Route::get('/subscriptions', [DashboardController::class, 'subscriptions'])->name("dashboard.subscriptions");
+
+
