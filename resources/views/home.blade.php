cfd7eaa62cb45a3540846e6369be3e2e62241203
Fildzaahns <sitifildzaah@gmail.com>
2021-05-24 01:04:36 +0700

    menambahkan informasi pada landing page ref #6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-<div class="pb-5">
+<div>
   <div class="mb-4 bg-yellow-100">
     <div class="container py-5">
       <div class="d-flex flex-column-reverse flex-lg-row align-items-center">
@@ -58,7 +58,83 @@
         </div>
       </div>
     </div>
+
+    <div class="row mb-5">
+      <div class="col-6">
+        <img src="/1.svg" height="200px">
+      </div>
+
+      <div class="col-6">
+        <h3>Kelebihan Menggunakan LIFE Insurance</h3>
+        <ul>
+          <li class="mb-4">Asuransi digital terpercaya di Indonesia</li>
+          <li class="mb-4">Asuransi digital temurah di Indonesia</li>
+          <li class="mb-4">Asuransi digital tercepat di Indonesia</li>
+          <li class="mb-4">Asuransi digital teraman di Indonesia</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="mb-5">
+      <h4>Testimoni</h4>
+      <div class="row">
+        <div class="col-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="mb-0" style="font-size: 14px">Komentar pengguna</p>
+              <p class="fw-bold fs-6 mb-0">Dadang subur</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="mb-0" style="font-size: 14px">Komentar pengguna</p>
+              <p class="fw-bold fs-6 mb-0">Rudi Tabuti</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="mb-0" style="font-size: 14px">Komentar pengguna</p>
+              <p class="fw-bold fs-6 mb-0">Anasya Prisilia</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="mb-0" style="font-size: 14px">Komentar pengguna</p>
+              <p class="fw-bold fs-6 mb-0">sedna athalla</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
   </div>
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <a href="/" class="mb-0 font-weight-bold navbar-brand text-warning">
+            <span class="fw-bold">
+              LIFE
+            </span>
+            Insurance +
+          </a>
+          <p class="mb-0">LIFE Insurance adalah perusahaan penyedia asuransi jiwa dan asuransi kesehatan</p>
+        </div>
+        <div class="col-6 text-end">
+          <p class="mb-0">Jalan Rasuna no 72 Jakarta 47564</p>
+          <p style="font-size: 13px">+62 28382382</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </div>
 
 @endsection
