391fd731b4182dbea72f5c3cde8041508afb89be
cantikadf <cantikadf16@gmail.com>
2021-05-24 12:27:10 +0700

    Menyelesaikan fitur ref#15

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Subscription;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -17,7 +18,8 @@ class UserController extends Controller
         // $policies = Policy::all();
         $subscriptions = Subscription::where('customer_id', $user->id)->get();
         $transactions = Transaction::where('customer_id', $user->id)->get();
+        $total = DB::table('transactions')->where('customer_id', $user->id)->where('status', 'paid')->sum('amount');
 
-        return view('profile', ['subscriptions' => $subscriptions, 'transactions' => $transactions]);
+        return view('profile', ['subscriptions' => $subscriptions, 'transactions' => $transactions, 'total' => $total]);
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Home')
-
+ 
 @section('content')
-
+ 
 <div class="container py-5" style="max-width: 800px">
   <div class="p-5 shadow card rounded-3">
     <div class="card-body">
@@ -23,17 +23,17 @@
           </tr>
         </thead>
         <tbody>
-          
-            @foreach($subscriptions as $subscription)
-            <tr>
+ 
+          @foreach($subscriptions as $subscription)
+          <tr>
             <td>{{ $subscription->policy_name }}</td>
             <td>{{ $subscription->premium }}</td>
             <td>
               <span class="badge bg-success">{{ $subscription->status }}</span>
             </td>
             <td>{{ $subscription->created_at }}</td>
-            
-
+ 
+ 
           </tr>
           @endforeach
         </tbody>
@@ -58,23 +58,30 @@
               <span class="badge bg-success">{{ $transaction->status }}</span>
             </td>
             @if ($transaction->status == 'paid')
-        <td>{{ $transaction->updated_at }}</td>
-        @else 
-        <td></td>
-        @endif
+            <td>{{ $transaction->updated_at }}</td>
+            @else
+            <td></td>
+            @endif
             <td>
-            @if ($transaction->status !='paid')
+              @if ($transaction->status !='paid')
               <a href="{{ route('transaction.detail', $transaction->id) }}" class="btn btn-sm btn-light">Bayar</a>
               @endif
             </td>
-
+ 
           </tr>
           @endforeach
+ 
+          @if($total > 0)
+          <tr>
+            <td colspan="3">Total Lunas</td>
+            <td>{{ $total }}</td>
+          </tr>
+          @endif
         </tbody>
       </table>
     </div>
-
+ 
   </div>
 </div>
-
+ 
 @endsection
